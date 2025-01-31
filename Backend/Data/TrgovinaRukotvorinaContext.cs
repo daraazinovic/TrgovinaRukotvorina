404c0b66fb87708991c7f4fd8f19b984b8b4a772
daraazinovic <darko.azinovic@gmail.com>
2025-01-31 18:59:18 +0100

    Baza

---
+++ Backend/Data/TrgovinaRukotvorinaContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Data
+{
+    public class TrgovinaRukotvorinaContext
+    {
+        public TrgovinaRukotvorinaContext(DbContextOptions<TrgovinaRukotvorinaContext> options) : base(options)
+        {
+
+        }
+        
+        public DbSet<Proizvod> Proizvodi { get; set; }
+    }
+
+    
+}
