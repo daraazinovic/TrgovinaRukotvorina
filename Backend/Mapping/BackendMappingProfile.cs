1873a38ebee019b7296b8d4afaf52d97bec08080
Darko Azinović <darko.azinovic@gmail.com>
2025-02-19 18:52:19 +0100

    update

---
+++ Backend/Mapping/BackendMappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using Backend.Models;
+
+namespace Backend.Mapping
+{
+    public class BackendMappingProfile : Profile
+    {
+
+        public BackendMappingProfile()
+        {
+            CreateMap<Proizvod, ProizvodDTORead>();
+            CreateMap<ProizvodDTOCreate, Proizvod>();
+
+            CreateMap<Materijal, MaterijalDTORead>()
+                .ForCtorParam(
+                "Sastav",
+                opt => opt.MapFrom(src => src.)
+        }
+    }
+}
